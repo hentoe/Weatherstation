4f29ae5c1708aee0b4dcef2d9dbdbb6b6888d500
hentoe <hendriktoedter@gmx.de>
2023-08-04 13:54:49 +0200

    Add filtering measurements by sensor id feature.

--- app/weatherstation/views.py
+++ app/weatherstation/views.py
@@ -1,6 +1,12 @@
 """
 Views for the weatherstation APIs.
 """
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -36,6 +42,17 @@ class SensorViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "sensors",
+                OpenApiTypes.STR,
+                description="Comma separated list of sensor IDs to filter",
+            )
+        ]
+    )
+)
 class MeasurementViewSet(viewsets.ModelViewSet):
     """View for managing measurement APIs."""
     serializer_class = serializers.MeasurementDetailSerializer
@@ -43,9 +60,20 @@ class MeasurementViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        """Convert a list of strings to integers."""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
         """Retrieve measurements for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+        sensors = self.request.query_params.get("sensors")
+        queryset = self.queryset
+        if sensors:
+            sensor_ids = self._params_to_ints(sensors)
+            queryset = queryset.filter(sensor__id__in=sensor_ids)
+        return queryset.filter(
+            user=self.request.user
+        ).order_by("-id").distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for requests."""
