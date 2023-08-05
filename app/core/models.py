14a881e508301996e14c99e240cea903c5afe492
hentoe <hendriktoedter@gmx.de>
2023-08-05 11:33:50 +0200

    Add Location and SensorType fields to Sensor model

--- app/core/models.py
+++ app/core/models.py
@@ -45,33 +45,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
 
 
-class Sensor(models.Model):
-    """Sensor object."""
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Measurement(models.Model):
-    """Measurement object."""
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    value = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.sensor} - {self.timestamp} - {self.value}"
-
-
 class Location(models.Model):
     """Location object."""
     user = models.ForeignKey(
@@ -95,3 +68,34 @@ class SensorType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Sensor(models.Model):
+    """Sensor object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    sensor_type = models.ForeignKey(
+        SensorType, on_delete=models.SET_NULL, null=True, blank=True)
+    location = models.ForeignKey(
+        Location, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    """Measurement object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.sensor} - {self.timestamp} - {self.value}"
