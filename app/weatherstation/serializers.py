be98435a5ea3f3ddda206543deb36b28f263dfe6
hentoe <hendriktoedter@gmx.de>
2023-08-08 11:52:58 +0200

    Implemented create sensor with sensor type feature

--- app/weatherstation/serializers.py
+++ app/weatherstation/serializers.py
@@ -9,38 +9,6 @@ from core.models import (
 )
 
 
-class SensorSerializer(serializers.ModelSerializer):
-    """Serializer for sensors."""
-
-    class Meta:
-        model = Sensor
-        fields = ["id", "name"]
-        read_only_fields = ["id"]
-
-
-class SensorDetailSerializer(SensorSerializer):
-    """Serializer for sensor detail view."""
-
-    class Meta(SensorSerializer.Meta):
-        fields = SensorSerializer.Meta.fields + ["description"]
-
-
-class MeasurementSerializer(serializers.ModelSerializer):
-    """Serializer for Measurements."""
-
-    class Meta:
-        model = Measurement
-        fields = ["id", "timestamp", "value"]
-        read_only_fields = ["id", "timestamp"]
-
-
-class MeasurementDetailSerializer(MeasurementSerializer):
-    """Serializer for measurement detail view."""
-
-    class Meta(MeasurementSerializer.Meta):
-        fields = MeasurementSerializer.Meta.fields + ["sensor"]
-
-
 class SensorTypeSerializer(serializers.ModelSerializer):
     """Serializer for SensorTypes."""
 
@@ -57,3 +25,56 @@ class LocationSerializer(serializers.ModelSerializer):
         model = Location
         fields = ["id", "name"]
         read_only_fields = ["id"]
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    """Serializer for sensors."""
+    sensor_type = SensorTypeSerializer(required=False)
+
+    class Meta:
+        model = Sensor
+        fields = ["id", "name", "sensor_type"]
+        read_only_fields = ["id"]
+
+    def _get_or_create_sensor_type(self, sensor_type, sensor):
+        """Handle getting or creating a sensor type."""
+        auth_user = self.context["request"].user
+        if sensor_type:
+            sensor_type_obj, created = SensorType.objects.get_or_create(
+                user=auth_user,
+                **sensor_type
+            )
+            sensor.sensor_type = sensor_type_obj
+
+    def create(self, validated_data):
+        """Create a sensor."""
+        sensor_type = validated_data.pop("sensor_type", None)
+        sensor = Sensor.objects.create(**validated_data)
+        self._get_or_create_sensor_type(sensor_type, sensor)
+
+        sensor.save()
+        return sensor
+
+
+class SensorDetailSerializer(SensorSerializer):
+    """Serializer for sensor detail view."""
+
+    class Meta(SensorSerializer.Meta):
+        fields = SensorSerializer.Meta.fields + \
+            ["description"]
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    """Serializer for Measurements."""
+
+    class Meta:
+        model = Measurement
+        fields = ["id", "timestamp", "value"]
+        read_only_fields = ["id", "timestamp"]
+
+
+class MeasurementDetailSerializer(MeasurementSerializer):
+    """Serializer for measurement detail view."""
+
+    class Meta(MeasurementSerializer.Meta):
+        fields = MeasurementSerializer.Meta.fields + ["sensor"]
