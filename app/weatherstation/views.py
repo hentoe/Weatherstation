c0c886ae2e55db0f251240488542e68a479908f7
hentoe <hendriktoedter@gmx.de>
2023-08-08 15:24:23 +0200

    Add filtering sensors feature.

--- app/weatherstation/views.py
+++ app/weatherstation/views.py
@@ -25,6 +25,22 @@ from core.models import (
 from weatherstation import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "locations",
+                OpenApiTypes.STR,
+                description="Comma separated list of location IDs to filter",
+            ),
+            OpenApiParameter(
+                "sensor_types",
+                OpenApiTypes.STR,
+                description="Comma separated list of sensortype IDs to filter",
+            )
+        ]
+    )
+)
 class SensorViewSet(viewsets.ModelViewSet):
     """View for managing sensor APIs."""
     serializer_class = serializers.SensorDetailSerializer
@@ -32,9 +48,25 @@ class SensorViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        """Convert a list of strings to integers."""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
         """Retrieve sensors for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+        locations = self.request.query_params.get("locations")
+        sensor_types = self.request.query_params.get("sensor_types")
+        queryset = self.queryset
+        if locations:
+            location_ids = self._params_to_ints(locations)
+            queryset = queryset.filter(location__id__in=location_ids)
+        if sensor_types:
+            sensor_type_ids = self._params_to_ints(sensor_types)
+            queryset = queryset.filter(sensor_type__id__in=sensor_type_ids)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by("-id").distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
