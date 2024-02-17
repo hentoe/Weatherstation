116b83b2b2fc9c49cf621400f32e5efd6f799468
hentoe <hendriktoedter@gmx.de>
2024-02-17 12:33:36 +0700

    Add custom domain for view site link in admin panel

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,8 @@
 """
 Django admin customizaiton.
 """
+import os
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
@@ -48,3 +50,4 @@ admin.site.register(models.Sensor)
 admin.site.register(models.Measurement)
 admin.site.register(models.Location)
 admin.site.register(models.SensorType)
+admin.site.site_url = os.environ.get(VUE_FRONTEND_DOMAIN)
