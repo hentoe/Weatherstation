d44fd00aed039f76501f1439c67f3acf425de403
hentoe <hendriktoedter@gmx.de>
2024-03-27 14:34:20 +0100

    Fix VUE_FRONTEND_DOMAIN. Add env variables for email to .env.sample

--- app/app/settings.py
+++ app/app/settings.py
@@ -166,7 +166,9 @@ SPECTACULAR_SETTINGS = {
 
 # Domain for djoser emails
 # Get rid of http:// or https:// from the env variable, as it is already part of the email template
-DOMAIN = os.environ.get("VUE_FRONTEND_DOMAIN").split("://")[1]
+DOMAIN = os.environ.get("VUE_FRONTEND_DOMAIN", "localhost:8000").split("://")[
+    -1
+]
 # Djoser settings
 DJOSER = {
     "PASSWORD_RESET_CONFIRM_URL": "#/password/reset/confirm/?uid={uid}&token={token}",
